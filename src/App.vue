aa361eca03f54f661dceb23de5a4fe7265d39428
Noor Akhyar <151505563+Akhyaru@users.noreply.github.com>
2023-11-27 16:41:48 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+//Testing
 import { useRouter } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
 
+// access the `store` variable anywhere in the component ✨
 const auth = useAuthStore();
 const router = useRouter();
 
@@ -11,52 +13,44 @@ const onLogout = () => {
 };
 
 const showAlert = () => {
-  alert("Anda harus login untuk mengakses halaman ini!");
+  alert("Anda harus login untuk mengakses halaman ini!"); // Menampilkan alert
 };
 </script>
 
 <template>
-  <div class="bg-blue-500">
-    <div class="container mx-auto p-4">
-      <div class="flex justify-between items-center">
-        <!-- Logo and Title -->
-        <div class="text-2xl font-bold text-white">Week 2</div>
-
-        <!-- Navigation Menu -->
-        <div class="flex gap-4">
-          <router-link to="/" class="text-white hover:underline">Home</router-link>
-          <router-link to="/about" class="text-white hover:underline">About</router-link>
-
-          <template v-if="auth.username">
-            <router-link to="/restricted" class="text-white hover:underline">Restricted Page</router-link>
-          </template>
-          
-          <template v-else>
-            <p class="text-white cursor-pointer" @click="showAlert">Restricted Page</p>
-          </template>
+  <div class="flex flex-col container mx-auto p-4 gap-10">
+    <!--Header-->
+    <div class="flex justify-between">
+      <!--Menu-->
+      <div class="flex gap-4">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link v-if="auth.username" to="/restricted"
+          >Restricted Page</router-link
+        >
+        <p v-else @click="showAlert">Restricted Page</p>
+      </div>
+      <!--Authenticated User-->
+      <div class="flex gap-2 items-center">
+        <p v-if="auth.username">{{ auth.username }}</p>
+        <div v-if="auth.username">
+          <button
+            class="bg-red-500 text-white py-1 px=3"
+            to="/login"
+            @click="onLogout()"
+          >
+            Logout
+          </button>
         </div>
 
-        <!-- Authenticated User -->
-        <div class="flex items-center gap-2">
-          <template v-if="auth.username">
-            <p class="text-white">{{ auth.username }}</p>
-            <button @click="onLogout" class="bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600">
-              Logout
-            </button>
-          </template>
-
-          <template v-else>
-            <router-link to="/login" class="bg-green-500 text-white py-1 px-3 rounded-md hover:bg-green-600">
-              Login
-            </router-link>
-          </template>
+        <div v-else>
+          <router-link class="bg-blue-500 text-white py-1 px-3" to="/login"
+            >Login</router-link
+          >
         </div>
       </div>
     </div>
-  </div>
-
-  <!-- Body -->
-  <div class="container mx-auto p-4">
+    <!--Body-->
     <router-view></router-view>
   </div>
 </template>
